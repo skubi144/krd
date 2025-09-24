caaa273d634f9a637c2d470fe6198f6381cae8e2
miloszskubala <miloszskubala@gmail.com>
2025-09-24 18:59:47 +0200

    extract tailwind

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,11 +2,9 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanstackDevtools } from '@tanstack/react-devtools'
 
-import Header from '@/shared/components/Header'
-
+import type { QueryClient } from '@tanstack/react-query'
 import TanStackQueryDevtools from '@/shared/integrations/tanstack-query/devtools'
 
-import type { QueryClient } from '@tanstack/react-query'
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -15,7 +13,6 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
-      <Header />
       <Outlet />
       <TanstackDevtools
         config={{
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
@@ -10,7 +9,6 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
-    tailwindcss(),
   ],
   test: {
     globals: true,
