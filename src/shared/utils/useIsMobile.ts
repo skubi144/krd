eb34f02f51829547c64b701d94068e23222589c9
miloszskubala <miloszskubala@gmail.com>
2025-10-04 09:07:56 +0200

    performance update

--- src/shared/utils/useIsMobile.ts
+++ src/shared/utils/useIsMobile.ts
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-const MOBILE_MAX = 767;
+const MOBILE_MAX = 767
 
-const getIsMobile = () => window.innerWidth <= MOBILE_MAX;
+const getIsMobile = () => window.innerWidth <= MOBILE_MAX
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = useState(getIsMobile());
+  const [isMobile, setIsMobile] = useState(() => getIsMobile())
 
   useEffect(() => {
-    const handleResize = () => setIsMobile(getIsMobile());
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    const handleResize = () => setIsMobile(getIsMobile())
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
-  return isMobile;
+  return isMobile
 }
