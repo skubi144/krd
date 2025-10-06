34e9f3378227746f84a00daf66063f6713594fa5
miloszskubala <miloszskubala@gmail.com>
2025-10-06 21:34:30 +0200

    add version into packagejson and index html

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,22 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vitest/config'
 import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import pkg from './package.json' with { type: 'json' }
 
+const { version } = pkg
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    {
+      name: 'html-version-comment',
+      apply: 'build',
+      transformIndexHtml(html) {
+        const comment = `<!-- App version: ${version} -->`
+        return `${html}\n${comment}`
+      },
+    },
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
